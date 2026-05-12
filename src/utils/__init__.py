5b66af0d8c50d8df27724db97996c948fbd60612
Overtek <theovertekgaming@gmail.com>
2026-05-12 16:56:07 +0200

    add pyproject, main script, display and .vscode settings for group project

---
+++ src/utils/__init__.py
@@ -0,0 +1,22 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  __init__.py                                       :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: anacharp, roandrie                        +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/05/12 16:53:55 by roandrie        #+#    #+#               #
+#  Updated: 2026/05/12 16:55:18 by roandrie        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+from src.utils.display import (print_error, print_log, print_rule,
+                               print_success)
+
+
+__all__ = [
+    "print_error",
+    "print_log",
+    "print_rule",
+    "print_success"
+]
---
+++ src/utils/display.py
@@ -0,0 +1,88 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  display.py                                        :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: anacharp, roandrie                        +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/27 09:46:37 by roandrie        #+#    #+#               #
+#  Updated: 2026/05/12 16:54:58 by roandrie        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+"""
+Terminal output management utilities.
+
+This module provides a centralized interface for displaying formatted
+messages using the Rich library. It manages distinct output streams
+(stdout and stderr) to ensure that logs and errors do not interfere with
+potential structured outputs.
+
+Attributes:
+    standard_console (Console): The default console instance for standard
+                                output.
+    error_console (Console): A console instance configured specifically for
+                             stderr.
+"""
+
+from rich.console import Console
+from rich.errors import StyleSyntaxError
+from rich.style import Style
+
+
+standard_console: Console = Console()
+error_console: Console = Console(stderr=True)
+
+
+def print_error(message: str) -> None:
+    """
+    Displays a formatted error message on the standard error stream.
+
+    Args:
+        message (str): The specific error description to be displayed.
+    """
+    prefix: str = "Error: "
+    content: str = f"{message}"
+    error_console.print(f"[bold red]{prefix + content}[/bold red]")
+
+
+def print_success(message: str) -> None:
+    """
+    Display a formatted success message on the standard stream.
+
+    Args:
+        message (str): The specific message to be displayed.
+    """
+    standard_console.print(f"[green]{message}[/green]")
+
+
+def print_log(message: str) -> None:
+    """
+    Display a formatted log message on the standard stream using 'log' from
+    rich. Using '_stack_offset' allow good naming for the file.
+
+    Args:
+        message (str): The specific message to be displayed.
+    """
+    standard_console.log(message, _stack_offset=2)
+
+
+def print_rule(message: str, color: str = "bold blue") -> None:
+    """
+    Display a horizontal rule with a message at the center and with a
+    specific color. If the color doesn't exist or isn't specify, the color
+    bold blue will by the default color.
+
+    Args:
+        message (str): The specific message to be displayed.
+        color (str): The color used for the rule.
+    """
+    style_rule: str = color
+
+    try:
+        Style.parse(style_rule)
+
+    except StyleSyntaxError:
+        style_rule = "bold blue"
+        print_error(f"'{color}' is unkown. Switching to default (bold blue).")
+
+    standard_console.rule(message, style=style_rule)
