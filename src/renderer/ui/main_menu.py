83aee50cb8ab419b233151b1f3cb82b3ea0e840e
Ana Charpentier <anacharp@c1r4p2.42lehavre.fr>
2026-05-15 18:35:30 +0200

    omg le menu

--- src/renderer/ui/main_menu.py
+++ src/renderer/ui/main_menu.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 19:37:31 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/15 14:05:44 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/15 18:33:56 by anacharp        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -18,6 +18,78 @@ from .base_menu import BaseMenu
 from .base_button import BaseButton
 from src.game_engine import GameEngine
 
+
+class LogoButton(BaseButton):
+    def __init__(
+            self, center_x: float,
+            center_y: float,
+            sprite_path: Path,
+        parent_view: arcade.View
+    ) -> None:
+
+        super().__init__(
+            center_x=center_x,
+            center_y=center_y,
+            sprite_path=sprite_path,
+            parent_view=parent_view
+        )
+
+    def on_click(self) -> None:
+        pass
+
+class ExitButton(BaseButton):
+    def __init__(
+            self, center_x: float,
+            center_y: float,
+            sprite_path: Path,
+        parent_view: arcade.View
+    ) -> None:
+
+        super().__init__(
+            center_x=center_x,
+            center_y=center_y,
+            sprite_path=sprite_path,
+            parent_view=parent_view
+        )
+
+    def on_click(self) -> None:
+        exit()
+
+class InstructionsButton(BaseButton):
+    def __init__(
+            self, center_x: float,
+            center_y: float,
+            sprite_path: Path,
+        parent_view: arcade.View
+    ) -> None:
+
+        super().__init__(
+            center_x=center_x,
+            center_y=center_y,
+            sprite_path=sprite_path,
+            parent_view=parent_view
+        )
+
+    def on_click(self) -> None:
+        pass
+
+class HighscoresButton(BaseButton):
+    def __init__(
+            self, center_x: float,
+            center_y: float,
+            sprite_path: Path,
+        parent_view: arcade.View
+    ) -> None:
+
+        super().__init__(
+            center_x=center_x,
+            center_y=center_y,
+            sprite_path=sprite_path,
+            parent_view=parent_view
+        )
+
+    def on_click(self) -> None:
+        pass
 
 class PlayButton(BaseButton):
     def __init__(
@@ -49,11 +121,44 @@ class MainMenu(BaseMenu):
 
     def build_ui(self) -> None:
 
+        logo_button = LogoButton(
+            center_x=640,
+            center_y=500,
+            sprite_path=self.window.asset_manager.textures["logo"],
+            parent_view=self
+        )
+
         play_button = PlayButton(
-            center_x=400,
-            center_y=300,
+            center_x=640,
+            center_y=400,
             sprite_path=self.window.asset_manager.textures["start_button"],
             parent_view=self
         )
 
+        highscores_button = HighscoresButton(
+            center_x=640,
+            center_y=300,
+            sprite_path=self.window.asset_manager.textures["highscores_button"],
+            parent_view=self
+        )
+
+        instructions_button = InstructionsButton(
+            center_x=640,
+            center_y=200,
+            sprite_path=self.window.asset_manager.textures["instructions_button"],
+            parent_view=self
+        )
+
+        exit_button = ExitButton(
+            center_x=640,
+            center_y=100,
+            sprite_path=self.window.asset_manager.textures["exit_button"],
+            parent_view=self
+        )
+
+        self.button_list.append(logo_button)
         self.button_list.append(play_button)
+        self.button_list.append(highscores_button)
+        self.button_list.append(instructions_button)
+        self.button_list.append(exit_button)
+
