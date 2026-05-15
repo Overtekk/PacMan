508f6c8f8d2c6fe99508bfb447e8059312e9f40e
Overtekk <theovertekgaming@gmail.com>
2026-05-15 16:44:57 +0200

    leaderboard logic: save player score, name can only be 10 characters, remove negative score, sort by highest score, keep only the 10 best score

--- src/leaderboard/__init__.py
+++ src/leaderboard/__init__.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 17:53:52 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/14 19:27:46 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/15 16:31:52 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -17,7 +17,7 @@ from .leaderboard_schema import (
     Leaderboard
 )
 from .update_leaderboard import (
-    save_score_to_leaderboard
+    save_score_to_leaderboard, open_leaderboard
 )
 
 
@@ -25,5 +25,6 @@ __all__ = [
     "leaderboard_loader",
     "create_leaderboard_file",
     "Leaderboard",
-    "save_score_to_leaderboard"
+    "save_score_to_leaderboard",
+    "open_leaderboard"
 ]
