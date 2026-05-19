a66d29469a9ebb15a4aa4116854951a635f2b01c
Overtekk <theovertekgaming@gmail.com>
2026-05-19 10:18:55 +0200

    level manager rebase'

--- src/game_engine/level_manager.py
+++ src/game_engine/level_manager.py
@@ -6,18 +6,25 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 20:04:01 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/18 16:11:31 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/19 10:17:55 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 from src.entity import Player
+from src.renderer.sprites_loader import
 
 
 class LevelManager():
     def __init__(self) -> None:
         pass
 
+    def create_level(
+        self, level_name: str, maze_width: int, maze_height: int
+    ) -> None:
+        pass
+
     def _create_entity(self) -> None:
+        # Create the player
         self.player = Player(
             spawn_point=(0, 0),
             sprite_sheet="",
