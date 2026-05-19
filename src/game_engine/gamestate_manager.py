f105a7898d0d44fe2680fbb24dd5c206aca32410
Ana Charpentier <anacharp@c1r4p2.42lehavre.fr>
2026-05-19 11:40:56 +0200

    menu pause fonctionnel

--- src/game_engine/gamestate_manager.py
+++ src/game_engine/gamestate_manager.py
@@ -1,19 +1,30 @@
 # ************************************************************************* #
 #                                                                           #
 #                                                      :::      ::::::::    #
-#  game_state.py                                     :+:      :+:    :+:    #
+#  gamestate_manager.py                              :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 20:03:38 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/14 21:33:31 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/19 11:28:13 by anacharp        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 import arcade
+from src.renderer.ui.pause_menu import PauseMenu
 
 
 class GameStateManager():
-    pass
+    def __init__(self, game_window: arcade.Window) -> None:
+        self.window = game_window
+        self.config = game_window.game_config
+        # self.asset_manager = game_window.sprites_list
+
+    def on_key_press(self, symbol: int, modifiers: int) -> None:
+        if symbol == arcade.key.ESCAPE:
+            arcade.exit()
+        if symbol == arcade.key.SPACE:
+            if self.window:
+                self.window.show_view(PauseMenu())
 
     # stock data : score, lives, current_level, timer
