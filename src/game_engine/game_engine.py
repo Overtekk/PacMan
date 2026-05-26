afd82a9615baea7f5b0a91a1b28f40fc8779c71c
Ana Charpentier <anacharp@c1r2p6.42lehavre.fr>
2026-05-26 12:22:40 +0200

    ui screen connecte et systeme de vies fonctionnel

--- src/game_engine/game_engine.py
+++ src/game_engine/game_engine.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 19:20:06 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/26 10:13:22 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/26 12:22:18 by anacharp        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -44,6 +44,11 @@ class GameEngine(arcade.View):
 
     def on_update(self, delta_time: float) -> None:
         self.game_renderer.update(delta_time)
+        self.game_renderer.update_ui(
+            self.state_manager.score,
+            self.state_manager.time_left,
+            self.state_manager.live
+        )
 
         if self.game_state == GameState.SETUP:
             pass
--- src/renderer/ui/ui_screen.py
+++ src/renderer/ui/ui_screen.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 19:44:37 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/22 15:52:34 by anacharp        ###   ########.fr        #
+#  Updated: 2026/05/26 12:19:46 by anacharp        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -15,7 +15,6 @@ import arcade
 from pathlib import Path
 
 from .base_menu import BaseMenu
-from .base_button import BaseButton
 from src.renderer.screen_settings import ScreenSettings
 
 
@@ -50,6 +49,8 @@ class UIScreen(BaseMenu):
         self.score = score
         self.time = time
         self.nb_lives = nb_lives
+        self.display_score = ""
+        self.display_time = ""
 
     def build_ui(self) -> None:
         x = 30
@@ -72,22 +73,51 @@ class UIScreen(BaseMenu):
                 y -= 50
                 x = 30
 
-        display_score = arcade.Text(text=f"Score: {self.score}",
-                                    x=ScreenSettings.WIDTH - 10,
-                                    y=ScreenSettings.HEIGHT - 10,
-                                    color=arcade.color.WHITE, font_size=20,
-                                    font_name="pressStart2P", anchor_x="right",
-                                    anchor_y="top")
-        self.text_lst.append(display_score)
-        display_time = arcade.Text(text=self.time, x=ScreenSettings.WIDTH -10,
-                                   y=10,
-                                   color=arcade.color.WHITE,
-                                   font_size=20, font_name="pressStart2P",
-                                   anchor_x="right", anchor_y="bottom")
-        self.text_lst.append(display_time)
+        self.display_score = arcade.Text(text=f"Score: {self.score}",
+                                         x=ScreenSettings.WIDTH // 2, y=10,
+                                         color=arcade.color.WHITE,
+                                         font_size=20,
+                                         font_name="pressStart2P",
+                                         anchor_x="center", anchor_y="bottom")
+        self.text_lst.append(self.display_score)
+        self.display_time = arcade.Text(text=self.time,
+                                        x=ScreenSettings.WIDTH - 10,
+                                        y=ScreenSettings.HEIGHT - 10,
+                                        color=arcade.color.WHITE,
+                                        font_size=20, font_name="pressStart2P",
+                                        anchor_x="right", anchor_y="top")
+        self.text_lst.append(self.display_time)
 
     def on_draw(self):
-        self.clear()
         self.button_list.draw()
         for txt in self.text_lst:
             txt.draw()
+
+    def update(self, score, time, live):
+        self.score = str(score)
+        self.time = str(time)
+        self.nb_lives = int(live)
+        if isinstance(self.display_score, arcade.Text):
+            self.display_score.text = f"Score: {self.score}"
+        if isinstance(self.display_time, arcade.Text):
+            self.display_time.text = self.time
+        self.button_list.clear()
+        x = 30
+        y = ScreenSettings.HEIGHT - 30
+        count = 0
+        for i in range (self.nb_lives):
+            count += 1
+            lives = DisplayLives(
+                center_x=x,
+                center_y=y,
+                sprite_path=(
+                    self.window.asset_manager.textures["life"]
+                ),
+                parent_view=self
+            )
+            self.button_list.append(lives)
+            x += 45
+            if count >= 3:
+                count = 0
+                y -= 50
+                x = 30
