5960c8b6a65988d49556e91b60f99adb44226d81
Overtekk <theovertekgaming@gmail.com>
2026-05-27 14:49:50 +0200

    refactor the config_loader

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/14 16:57:57 by roandrie        #+#    #+#               #
-#  Updated: 2026/05/27 14:32:52 by roandrie        ###   ########.fr        #
+#  Updated: 2026/05/27 14:49:25 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -14,82 +14,82 @@ from pathlib import Path
 
 from .config_schema import GameConfig
 
-from src.utils import can_read_file, can_write_to_file
-from src.utils import is_folder_exist, is_file_exist, check_file_extension
+from src.utils import (
+    can_read_file, can_write_to_file, is_folder_exist, is_file_exist,
+    check_file_extension
+    )
 from src.utils.display import print_error
 
 
 mandatory_keys: list[str] = [
-    "highscore_filename", "live", "pacgum_points", "super_pacgum_points",
-    "ghost_points", "seed", "level_max_time", "level"
+    "highscore_filename",
+    "live",
+    "pacgum_points",
+    "super_pacgum_points",
+    "ghost_points",
+    "seed",
+    "level_max_time",
+    "level"
 ]
 
-
-
 def load_config(filepath: Path) -> GameConfig:
+    # SECURITY: create the Path object
     filepath: Path = Path(filepath)
 
+    # Check 'data' folder
     if not is_folder_exist(filepath.parent):
-        print_error(
-            f"'{filepath.parent}' does not exist.\nLet's play with default "
-            "config"
-        )
-        default_config: GameConfig = _create_config()
-        return default_config
 
+        _print_default_error_message(f"'{filepath.parent}' doesn't exist.")
+        return _create_default_config()
+
+    # Check permissions of 'data' folder
     if (not can_read_file(filepath.parent) or not
             can_write_to_file(filepath.parent)):
-        raise ValueError(f"Missing permissions for: '{filepath.parent}'")
 
+        raise ValueError(f"Missing permissions for '{filepath.parent}'")
+
+    # Check if config file exist
     if not is_file_exist(filepath):
-        print_error(
-            f"'{filepath}' does not exist.\nLet's play with default config"
-        )
-        default_config: GameConfig = _create_config()
-        return default_config
 
+        _print_default_error_message(f"'{filepath}' doesn't exist.")
+        return _create_default_config()
+
+    # Check if config file have json extension
     if not check_file_extension(filepath, 'json'):
-        print_error(
-            f"'{filepath}' is not a .json.\nLet's play with default config"
-        )
-        default_config: GameConfig = _create_config()
-        return default_config
 
+        _print_default_error_message(f"'{filepath}' is not '.json'")
+        return _create_default_config()
+
+    # Check permission of config file
     if not can_read_file(filepath) or not can_write_to_file(filepath):
-        print_error(
-            f"Not all permissions are granted for: '{filepath}'.\n"
-            "Let's play with default config"
-        )
-        default_config: GameConfig = _create_config()
-        return default_config
+
+        _print_default_error_message(f"Permission error for '{filepath}'.")
+        return _create_default_config()
 
     # Check for missing keys
     missing_keys: list[str] = []
 
-    for k in mandatory_keys:
-        if k not in filepath.read_text():
-            missing_keys.append(k)
+    for key in mandatory_keys:
+        if key not in filepath.read_text():
+            missing_keys.append(key)
 
     if missing_keys:
-        print_error(
-            f"Missing key/s : {missing_keys}.\nLet's play with default config"
-        )
-        default_config = _create_config()
-        return default_config
+        _print_default_error_message(f"Missing mandatory keys {missing_keys}.")
+        return _create_default_config()
 
-    # If no errors, check is json is valid
+    # Pydantic validation
     try:
-        config: GameConfig = (
-            GameConfig.model_validate_json(filepath.read_text())
-        )
-
+        return GameConfig.model_validate_json(filepath.read_text())
     except Exception as e:
-        print_error(f"{e}\nLet's play with default config")
-        default_config: GameConfig = _create_config()
-        return default_config
 
-    return config
+        _print_default_error_message(f"Configuration error: {e}")
+        return _create_default_config()
 
 
-def _create_config() -> GameConfig:
+def _create_default_config() -> GameConfig:
     return GameConfig()
+
+def _print_default_error_message(error: str) -> None:
+    error += "\nLet's play with the default config ! 🐨"
+
+    print_error(error)
