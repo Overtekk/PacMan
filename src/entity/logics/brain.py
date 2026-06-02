83622bad6dd3593365ed86317228789244e2e71c
Ana Charpentier <anacharp@c1r2p6.42lehavre.fr>
2026-06-02 14:31:01 +0200

    vitesse adaptee runaway et chase

--- src/entity/logics/brain.py
+++ src/entity/logics/brain.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/05/29 14:10:28 by roandrie        #+#    #+#               #
-#  Updated: 2026/06/02 10:59:00 by anacharp        ###   ########.fr        #
+#  Updated: 2026/06/02 14:27:28 by anacharp        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -20,7 +20,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from src.entity.entity import Enemy
 
-CHASE_SPEED: float = game_config.chase_speed
+# CHASE_SPEED: float = game_config.chase_speed
 
 
 class EnemyBrain():
@@ -45,7 +45,7 @@ class EnemyBrain():
             pass
 
         elif self.enemy.mode == EnemyState.CHASE:
-            self.enemy.speed = self.enemy.base_speed + CHASE_SPEED
+            self.enemy.speed = self.enemy.base_speed + (self.enemy.base_speed//10)
             self._chase_player(delta_time)
 
         elif self.enemy.mode == EnemyState.RUNAWAY:
@@ -173,7 +173,7 @@ class EnemyBrain():
             if self.enemy._timer_chase > MAX_TIME_TO_FORGET:
 
                 if random.random() <= self.enemy._loose_chance:
-                    self.enemy.speed = self.enemy.base_speed - CHASE_SPEED
+                    self.enemy.speed = self.enemy.base_speed - (self.enemy.base_speed//10)
 
                     self.enemy.mode = EnemyState.WANDER
                     self.enemy._timer_chase = 0.0
