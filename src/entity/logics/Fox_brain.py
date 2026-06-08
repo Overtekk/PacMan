4f331a789d80ee125d36cbd221459827f4d5f60d
Overtekk <theovertekgaming@gmail.com>
2026-06-08 13:53:17 +0200

    change radius

--- src/entity/logics/Fox_brain.py
+++ src/entity/logics/Fox_brain.py
@@ -6,7 +6,7 @@
 #  By: anacharp, roandrie                        +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/06/08 11:42:52 by roandrie        #+#    #+#               #
-#  Updated: 2026/06/08 13:49:33 by roandrie        ###   ########.fr        #
+#  Updated: 2026/06/08 13:53:09 by roandrie        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -83,4 +83,3 @@ class FoxBrain(EnemyBrain):
         self.detection_radius: float = (
             self.radius * self.enemy.calculator.maze_tile_size
         )
-        print(self.detection_radius)
